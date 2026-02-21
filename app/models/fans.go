e31eb86987e8b703617ce856a4db55a6bfd330c5
Kiprono Alex <beralynebera@gmail.com>
2026-02-22 02:00:34 +0300

    added redis

--- app/models/fans.go
+++ app/models/fans.go
@@ -5,9 +5,9 @@ import "time"
 type Fan struct {
 	BaseModel
 	Name         string    `json:"name" gorm:"not null"`
-	Email        string    `json:"email" gorm:"uniqueIndex"`
+	Email        string    `json:"email" gorm:"type:varchar(191);uniqueIndex"`
 	Tier         string    `json:"tier"`
 	JoinDate     time.Time `json:"join_date"`
 	Location     string    `json:"location"`
-	MembershipID string    `json:"membership_id" gorm:"uniqueIndex"`
+	MembershipID string    `json:"membership_id" gorm:"type:varchar(191);uniqueIndex"`
 }
