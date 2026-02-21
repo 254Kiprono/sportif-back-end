70014a768fa28ebe38d791d129cea8615881b8ed
Kiprono Alex <beralynebera@gmail.com>
2026-02-22 00:14:35 +0300

    added redis

--- app/models/role.go
+++ app/models/role.go
@@ -2,13 +2,13 @@ package models
 
 type Role struct {
 	BaseModel
-	Name        string       `json:"name" gorm:"uniqueIndex;not null"`
+	Name        string       `json:"name" gorm:"type:varchar(255);uniqueIndex;not null"`
 	Description string       `json:"description"`
 	Permissions []Permission `json:"permissions" gorm:"many2many:role_permissions;"`
 }
 
 type Permission struct {
 	BaseModel
-	Name        string `json:"name" gorm:"uniqueIndex;not null"`
+	Name        string `json:"name" gorm:"type:varchar(255);uniqueIndex;not null"`
 	Description string `json:"description"`
 }
--- app/models/user.go
+++ app/models/user.go
@@ -7,9 +7,9 @@ import (
 type User struct {
 	BaseModel
 	FullName string    `json:"full_name" gorm:"not null"`
-	Username string    `json:"username" gorm:"uniqueIndex;not null"`
-	Email    string    `json:"email" gorm:"uniqueIndex;not null"`
-	Phone    string    `json:"phone" gorm:"uniqueIndex;not null"`
+	Username string    `json:"username" gorm:"type:varchar(255);uniqueIndex;not null"`
+	Email    string    `json:"email" gorm:"type:varchar(255);uniqueIndex;not null"`
+	Phone    string    `json:"phone" gorm:"type:varchar(255);uniqueIndex;not null"`
 	Password string    `json:"-" gorm:"not null"`
 	RoleID   uuid.UUID `json:"role_id" gorm:"type:char(36);not null"`
 	Role     Role      `json:"role" gorm:"foreignKey:RoleID"`
